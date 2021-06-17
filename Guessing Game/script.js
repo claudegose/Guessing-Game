c9ffe8a24ebecc98622be241b093bfac716afa4c
claudegose <84382636+claudegose@users.noreply.github.com>
2021-06-17 21:20:30 +0200

    Edits

--- Guessing Game/script.js
+++ Guessing Game/script.js
@@ -1,21 +1,15 @@
 
-let x = Math.floor(Math.random() * 15 + 1);
-
-document.getElementById("submitguess").onclick = function () {
-    let y = document.getElementById("guessField").value;
 
 
-    if ( x == y ){
-        alert(`Congratulations! Your guess ${y} was correct`)
-    } else {
-        alert(`Sorry! You guessed ${y} and the correct number was ${x}! Try again`)
-    }
+let randomNumber = Math.floor(Math.random() * 15 + 1);
 
+   document.getElementById("submitguess").onclick = function () {
+     let userNumber =  document.getElementById("guessField").value;
 
+     if (randomNumber == userNumber){
+         alert(`Congratulations, You picked ${userNumber}!`)
+     } else {
+         alert(`Sorry! You chose ${userNumber} and the correct number was ${randomNumber}. Try again!`);
+     }
 
-
-
-
-
-
-}
+   }
