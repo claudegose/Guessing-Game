e1dd8e6dc3a983970b41939eaf2fcb9eea933a46
claudegose <84382636+claudegose@users.noreply.github.com>
2021-06-21 10:50:57 +0200

    + 1 - 1 Working

--- Guessing Game/script.js
+++ Guessing Game/script.js
@@ -6,10 +6,14 @@ document.getElementById("btn").onclick = () => {
     if (computerGenerated == userGuess ){
         alert(`Awesome! You number ${userGuess} was correct. You can be named many things, hungry not being one of them.`);
 
-    } else {
+    } else if(computerGenerated - userGuess === 1) {
+        alert('Try a higher number')
+    } else if ( userGuess - computerGenerated === 1){
+        alert('Try a lower number')
+    }
+    else {
         alert(`Bummer... You guessed ${userGuess} and the secret number was ${computerGenerated}.`)
     }
-
 }
 
 
