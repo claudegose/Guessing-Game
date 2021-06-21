7336ff69e16d5d0f928e9616f66a23cbdf0758ef
claudegose <84382636+claudegose@users.noreply.github.com>
2021-06-21 10:28:19 +0200

    Deployed New Method

--- Guessing Game/script.js
+++ Guessing Game/script.js
@@ -1,17 +1,34 @@
+let computerGenerated = Math.floor(Math.random() * 15 + 1);
+
+document.getElementById("btn").onclick = () => {
+    let userGuess = document.getElementById("userNumber").value;
+
+    if (computerGenerated == userGuess ){
+        alert(`Awesome! You number ${userGuess} was correct. You can be named many things, hungry not being one of them.`);
+
+    } else {
+        alert(`Bummer... You guessed ${userGuess} and the secret number was ${computerGenerated}.`)
+    }
+
+}
+
+
+/*
+
 let computer = Math.floor(Math.random() * 15 + 1);
 
 document.querySelector("#btn").onclick = () => {
     let user = document.querySelector("#userGuess").value;
 
     if (user == computer) {
-      document.querySelector("#results").innerHTML = document.write = `Awesome! You number ${user} was correct. You can be named many things, hungry not being one of them.`;
+        document.querySelector("#results").innerHTML = document.write = `Awesome! You number ${user} was correct. You can be named many things, hungry not being one of them.`;
     } else if (user < computer){
         alert("Try a higher number!")
-    }   else if (user > computer)
-    {alert("Try a lower number ")
+    }   else if (user > computer) {
+        alert("Try a lower number ")
     }
-
-
 }
 
+
+*/
 
